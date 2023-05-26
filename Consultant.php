ff085fc0573a94ecc7a7c50be36ab2ca887b55e4
Soraxy <106920748+Soraxy@users.noreply.github.com>
2023-05-26 16:13:50 +0200

    c le début

---
+++ Consultant.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="fr" lang="fr">
+<head>
+    <meta charset="utf-8" http-equiv="content-type" content="text/html;charset=iso-8859-1"/>
+</head>
+<link rel="stylesheet" href="Consultant.css">
+
+<body>
+
+<div id="head_title">
+    <img id=img src="LOGOS_JEUNES_1.png" alt="" />
+    <div class="head_text">
+        <h1>CONSULTANT <br>Je donne de la valeur à ton engagement</h1>
+</div>
+</div>
+
+<div id="sub_title">
+    <a class="Jeune" href="Jeune.php">JEUNE</a>
+    <a class="Référent" href="Référent.php">REFERENT</a>
+    <a class="Consultant" href="Consultant.php">CONSULTANT</a>
+    <a class="Partenaire" href="Partenaires.php">PARTENAIRES</a>
+</div><br><br>  
